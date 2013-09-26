dd39bcd1f16c0f2869d235daae0b6961d46a984c
Marek Kubica <marek@xivilization.net>
2013-09-26 21:49:32 +0200

    Added functionality of doc without print-doc
    Also, without macros. Just use symbols.

--- src/informant/core.clj
+++ src/informant/core.clj
@@ -2,5 +2,10 @@
 
 (defn inform
   "Returns the doc"
-  [x]
-  {})
+  [sym]
+  (if-let [special-name ('{& fn catch try finally try} sym)]
+    (@#'clojure.repl/special-doc special-name)
+    (cond
+      (@#'clojure.repl/special-doc-map sym) (@#'clojure.repl/special-doc-map sym)
+      (find-ns sym) (@#'clojure.repl/namespace-doc (find-ns sym))
+      (resolve sym) (meta (resolve sym)))))
