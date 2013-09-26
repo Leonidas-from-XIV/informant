c4bc03f47a1edf138a8701d2b086b05f0ebd473d
Marek Kubica <marek@xivilization.net>
2013-09-26 17:39:47 +0200

    A new project, generated by lein new

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject informant "0.1.0-SNAPSHOT"
+  :description "Introspect documentation information"
+  :url "https://github.com/Leonidas-from-XIV/informant"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]])
